6db21d2d8c356ae5835817a9f991118fd0d7e942
ImKventis <kventis.gg@gmail.com>
2023-10-06 11:03:55 +0100

    Added Circle Shape & unit

--- OopShapeTest/UnitTest1.cs
+++ OopShapeTest/UnitTest1.cs
@@ -4,6 +4,8 @@ using OopShapeDemo;
 
 namespace OopShapeTest
 {
+
+
     [TestClass]
     public class UnitTest1
     {
@@ -30,6 +32,29 @@ namespace OopShapeTest
         public void IcosagonConstructorNegativeTest()
         {
             Assert.ThrowsException<ArgumentException>(() => { new Icosagon(-1); }, "Shape did not throw expected arguement when negative value passed to constructor..");
+        }
+
+        [TestCategory("Circle.cs")]
+        [TestMethod]
+        // Test Circle can calculate area correctly.
+        public void CircleAreaTest()
+        {
+            double area = new Circle(30).Area();
+            Assert.AreEqual(706.86, Math.Round(area, 2, MidpointRounding.AwayFromZero));
+        }
+        
+        [TestMethod]
+        // Test Circle throw error when sent 0 as an arguement for length.
+        public void CircleConstructorZeroTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => { new Circle(0); }, "Shape did not throw expected arguement when zero passed to constructor.");
+        }
+
+        [TestMethod]
+        // Test Circle throw error when sent a negative number as an arguement for length.
+        public void CircleConstructorNegativeTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => { new Circle(-1); }, "Shape did not throw expected arguement when negative value passed to constructor..");
         }
     }
 }
